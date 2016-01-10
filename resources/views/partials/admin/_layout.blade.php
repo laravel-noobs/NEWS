ab25e899356c23f5344d954df4baa92969f37b2e
GiangIT <nhokgiang1994@gmail.com>
2016-01-10 14:42:24 +0700

    fix lỗi toarst vì đưa script flashmessage lên trên head

--- resources/views/partials/admin/_layout.blade.php
+++ resources/views/partials/admin/_layout.blade.php
@@ -37,5 +37,11 @@
     </div>
 </div>
 @yield('footer-script')
+<script>
+    if(typeof(flash_messages) !== 'undefined')
+        if(flash_messages != null)
+            for(i = 0; i < flash_messages.length; i++)
+                toastr[flash_messages[i]['type']](flash_messages[i]['message'], flash_messages[i]['title'], flash_messages[i]['options']);
+</script>
 </body>
 </html>
