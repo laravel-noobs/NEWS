a9c6f639bda2b0c0b9d0734a4fc303c700e6a4e4
Kou Tsuneka <tsuneka.kou@gmail.com>
2016-01-19 18:43:21 +0700

    flashmessage dùng view của vendor, composer update

--- resources/views/partials/admin/_layout.blade.php
+++ resources/views/partials/admin/_layout.blade.php
@@ -35,11 +35,6 @@
     </div>
 </div>
 @yield('footer-script')
-<script>
-    if(typeof(flash_messages) !== 'undefined')
-        if(flash_messages != null)
-            for(i = 0; i < flash_messages.length; i++)
-                toastr[flash_messages[i]['type']](flash_messages[i]['message'], flash_messages[i]['title'], flash_messages[i]['options']);
-</script>
+@include('_flashmessage::_script')
 </body>
 </html>
